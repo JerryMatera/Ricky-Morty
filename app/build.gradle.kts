95f22d898958d08295568002f6e806e313f7f86b
JerryMatera <jerryonyango8@gmail.com>
2023-01-18 23:42:05 +0300

    Update gradle deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,8 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 
     // pagination
     implementation("androidx.paging:paging-compose:1.0.0-alpha17")
