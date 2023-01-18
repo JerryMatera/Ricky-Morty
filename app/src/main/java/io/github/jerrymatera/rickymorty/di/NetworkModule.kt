b539e5600b4b6e3ee642d34b66d7fa113cba848e
JerryMatera <jerryonyango8@gmail.com>
2023-01-18 23:53:42 +0300

    implement and provide RickyMortyService

---
+++ app/src/main/java/io/github/jerrymatera/rickymorty/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package io.github.jerrymatera.rickymorty.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.jerrymatera.rickymorty.data.remote.RickyMortyService
+import io.github.jerrymatera.rickymorty.utils.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRickyMortyService(okHttpClient: OkHttpClient, moshi: Moshi): RickyMortyService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(RickyMortyService::class.java)
+    }
+
+}
