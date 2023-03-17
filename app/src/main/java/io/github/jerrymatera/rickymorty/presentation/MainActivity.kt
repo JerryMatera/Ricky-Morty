6226a632e3a82f2dd42c6ee3816285dd0d3c14f8
JerryMatera <jerryonyango8@gmail.com>
2023-03-17 22:45:58 +0300

    Install SplashScreen

--- app/src/main/java/io/github/jerrymatera/rickymorty/presentation/MainActivity.kt
+++ app/src/main/java/io/github/jerrymatera/rickymorty/presentation/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.jerrymatera.rickymorty.presentation.ui.theme.RickyMortyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition { true }
         super.onCreate(savedInstanceState)
         setContent {
             RickyMortyTheme {
